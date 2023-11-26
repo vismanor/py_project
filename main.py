85d37dc34876f9e199cf3df3e89d6369cbac81c5
Katya Smirnova <vismanork@gmail.com>
2023-11-26 23:56:20 +0300

    Импорт и вызов функции вычитания

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from funcs import summarise
+from funcs import summarise, subtract
 
 a = 3
 b = 7
 result = summarise(a, b)
+print(result)
+
+a = 255
+b = 8
+result = subtract(a, b)
 print(result)
